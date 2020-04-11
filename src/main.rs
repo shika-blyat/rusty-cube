d63060b6cdf75a7e8507415027c18e29afa97ade
shika-blyat <abdelzighem1@gmail.com>
2020-04-11 02:05:39 +0200

    add clear color

---
+++ src/main.rs
@@ -0,0 +1,147 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+const COLORS: [wgpu::Color; 3] = [
+    wgpu::Color {
+        r: 0.5,
+        g: 0.2,
+        b: 0.1,
+        a: 1.0,
+    },
+    wgpu::Color {
+        r: 0.1,
+        g: 0.2,
+        b: 0.3,
+        a: 1.0,
+    },
+    wgpu::Color {
+        r: 1.0,
+        g: 0.2,
+        b: 0.5,
+        a: 0.8,
+    },
+];
+struct State {
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+    clear_color: usize,
+}
+impl State {
+    fn new(window: &Window) -> Self {
+        let size = window.inner_size();
+
+        let surface = wgpu::Surface::create(window);
+
+        let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
+            ..Default::default()
+        })
+        .unwrap();
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            extensions: wgpu::Extensions {
+                anisotropic_filtering: false,
+            },
+            limits: Default::default(),
+        });
+        let sc_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Vsync,
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+        Self {
+            surface,
+            adapter,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+            clear_color: 0,
+        }
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.size = new_size;
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
+        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+    fn update_color(&mut self, f: impl Fn(usize) -> usize) {
+        self.clear_color = f(self.clear_color);
+    }
+    fn update(&mut self) {}
+
+    fn render(&mut self) {
+        let frame = self.swap_chain.get_next_texture();
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
+        let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                attachment: &frame.view,
+                resolve_target: None,
+                load_op: wgpu::LoadOp::Clear,
+                store_op: wgpu::StoreOp::Store,
+                clear_color: COLORS[self.clear_color],
+            }],
+            depth_stencil_attachment: None,
+        });
+        std::mem::drop(_render_pass);
+        self.queue.submit(&[encoder.finish()])
+    }
+}
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let mut state = State::new(&window);
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            if state.input(event) {
+                *control_flow = ControlFlow::Wait;
+            } else {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
+                    WindowEvent::KeyboardInput { input, .. } => match input {
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        _ => *control_flow = ControlFlow::Wait,
+                    },
+
+                    _ => *control_flow = ControlFlow::Wait,
+                }
+            }
+        }
+        Event::MainEventsCleared => {
+            state.update();
+            state.render();
+            *control_flow = ControlFlow::Wait;
+        }
+        _ => *control_flow = ControlFlow::Wait,
+    });
+}
