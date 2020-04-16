4eae1e0847b434ab58effa4225e8cf522be79e36
Shika-Blyat <abdelzighem1@gmail.com>
2020-04-16 06:06:55 +0200

    Add freetype dependency and refactor a little bit

--- src/old_main.rs
+++ src/old_main.rs
@@ -1,5 +1,4 @@
 use winit::{
-    dpi::PhysicalSize,
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
@@ -36,31 +35,35 @@ impl Vertex {
 
 const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-0.5, -0.5, 0.0],
-        tex_coords: [0.0, 0.0],
-    },
+        position: [-0.0868241, -0.49240386, 0.0],
+        tex_coords: [0.4131759, 0.00759614],
+    }, // A
     Vertex {
-        position: [0.5, -0.5, 0.0],
-        tex_coords: [2.0, 0.0],
-    },
+        position: [-0.49513406, -0.06958647, 0.0],
+        tex_coords: [0.0048659444, 0.43041354],
+    }, // B
     Vertex {
-        position: [0.5, 0.5, 0.0],
-        tex_coords: [2.0, 2.0],
-    },
+        position: [-0.21918549, 0.44939706, 0.0],
+        tex_coords: [0.28081453, 0.949397057],
+    }, // C
     Vertex {
-        position: [-0.5, 0.5, 0.0],
-        tex_coords: [0.0, 2.0],
-    },
+        position: [0.35966998, 0.3473291, 0.0],
+        tex_coords: [0.85967, 0.84732911],
+    }, // D
+    Vertex {
+        position: [0.44147372, -0.2347359, 0.0],
+        tex_coords: [0.9414737, 0.2652641],
+    }, // E
 ];
 
-const INDICES: &[u16] = &[2, 1, 0, 3, 2, 0];
-
+const INDICES: &[u16] = &[0, 1, 2, 1, 2, 4, 2, 3, 4];
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
     queue: wgpu::Queue,
-    swap_chain: wgpu::SwapChain,
     sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+
     render_pipeline: wgpu::RenderPipeline,
 
     vertex_buffer: wgpu::Buffer,
@@ -93,98 +96,6 @@ impl State {
             limits: Default::default(),
         });
 
-        let (sc_desc, swap_chain) = Self::create_swap_chain(&device, size, &surface);
-        let (
-            size3d,
-            diffuse_texture,
-            diffuse_buffer,
-            diffuse_sampler,
-            diffuse_texture_view,
-            diffuse_bind_group,
-            texture_bind_group_layout,
-        ) = Self::create_texture_stuff(&device, &mut queue);
-        let render_pipeline = Self::create_pipeline(&device, &sc_desc, &texture_bind_group_layout);
-        let vertex_buffer = device
-            .create_buffer_mapped(VERTICES.len(), wgpu::BufferUsage::VERTEX)
-            .fill_from_slice(VERTICES);
-        let index_buffer = device
-            .create_buffer_mapped(INDICES.len(), wgpu::BufferUsage::INDEX)
-            .fill_from_slice(INDICES);
-        let num_indices = INDICES.len() as u32;
-
-        Self {
-            surface,
-            device,
-            queue,
-            sc_desc,
-            swap_chain,
-            render_pipeline,
-            vertex_buffer,
-            index_buffer,
-            num_indices,
-            diffuse_texture,
-            diffuse_texture_view,
-            diffuse_sampler,
-            diffuse_bind_group,
-            size,
-        }
-    }
-    fn create_pipeline(
-        device: &wgpu::Device,
-        sc_desc: &wgpu::SwapChainDescriptor,
-        texture_bind_group_layout: &wgpu::BindGroupLayout,
-    ) -> wgpu::RenderPipeline {
-        let vs_src = include_str!("vert.glsl");
-        let fs_src = include_str!("frag.glsl");
-        let vs_spirv = glsl_to_spirv::compile(vs_src, glsl_to_spirv::ShaderType::Vertex).unwrap();
-        let fs_spirv = glsl_to_spirv::compile(fs_src, glsl_to_spirv::ShaderType::Fragment).unwrap();
-        let vs_data = wgpu::read_spirv(vs_spirv).unwrap();
-        let fs_data = wgpu::read_spirv(fs_spirv).unwrap();
-        let vs_module = device.create_shader_module(&vs_data);
-        let fs_module = device.create_shader_module(&fs_data);
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                bind_group_layouts: &[&texture_bind_group_layout],
-            });
-
-        device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            layout: &render_pipeline_layout,
-            vertex_stage: wgpu::ProgrammableStageDescriptor {
-                module: &vs_module,
-                entry_point: "main",
-            },
-            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
-                module: &fs_module,
-                entry_point: "main",
-            }),
-            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: wgpu::CullMode::Back,
-                depth_bias: 0,
-                depth_bias_slope_scale: 0.0,
-                depth_bias_clamp: 0.0,
-            }),
-            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-            color_states: &[wgpu::ColorStateDescriptor {
-                format: sc_desc.format,
-                color_blend: wgpu::BlendDescriptor::REPLACE,
-                alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                write_mask: wgpu::ColorWrite::ALL,
-            }],
-            depth_stencil_state: None,
-            index_format: wgpu::IndexFormat::Uint16,
-            vertex_buffers: &[Vertex::desc()],
-            sample_count: 1,
-            sample_mask: !0,
-            alpha_to_coverage_enabled: false,
-        })
-    }
-    fn create_swap_chain(
-        device: &wgpu::Device,
-        size: PhysicalSize<u32>,
-        surface: &wgpu::Surface,
-    ) -> (wgpu::SwapChainDescriptor, wgpu::SwapChain) {
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
@@ -192,21 +103,8 @@ impl State {
             height: size.height,
             present_mode: wgpu::PresentMode::Vsync,
         };
-        let swapchain = device.create_swap_chain(surface, &sc_desc);
-        (sc_desc, swapchain)
-    }
-    fn create_texture_stuff(
-        device: &wgpu::Device,
-        queue: &mut wgpu::Queue,
-    ) -> (
-        wgpu::Extent3d,
-        wgpu::Texture,
-        wgpu::Buffer,
-        wgpu::Sampler,
-        wgpu::TextureView,
-        wgpu::BindGroup,
-        wgpu::BindGroupLayout,
-    ) {
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
         let diffuse_bytes = include_bytes!("../happy-tree.png");
         let diffuse_image = image::load_from_memory(diffuse_bytes).unwrap();
         let diffuse_rgba = diffuse_image.as_rgba8().unwrap();
@@ -299,22 +197,92 @@ impl State {
                 },
             ],
         });
-        (
-            size3d,
+
+        let vs_src = include_str!("vert.glsl");
+        let fs_src = include_str!("frag.glsl");
+        let vs_spirv = glsl_to_spirv::compile(vs_src, glsl_to_spirv::ShaderType::Vertex).unwrap();
+        let fs_spirv = glsl_to_spirv::compile(fs_src, glsl_to_spirv::ShaderType::Fragment).unwrap();
+        let vs_data = wgpu::read_spirv(vs_spirv).unwrap();
+        let fs_data = wgpu::read_spirv(fs_spirv).unwrap();
+        let vs_module = device.create_shader_module(&vs_data);
+        let fs_module = device.create_shader_module(&fs_data);
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                bind_group_layouts: &[&texture_bind_group_layout],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            layout: &render_pipeline_layout,
+            vertex_stage: wgpu::ProgrammableStageDescriptor {
+                module: &vs_module,
+                entry_point: "main",
+            },
+            fragment_stage: Some(wgpu::ProgrammableStageDescriptor {
+                module: &fs_module,
+                entry_point: "main",
+            }),
+            rasterization_state: Some(wgpu::RasterizationStateDescriptor {
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: wgpu::CullMode::Back,
+                depth_bias: 0,
+                depth_bias_slope_scale: 0.0,
+                depth_bias_clamp: 0.0,
+            }),
+            primitive_topology: wgpu::PrimitiveTopology::TriangleList,
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
+            depth_stencil_state: None,
+            index_format: wgpu::IndexFormat::Uint16,
+            vertex_buffers: &[Vertex::desc()],
+            sample_count: 1,
+            sample_mask: !0,
+            alpha_to_coverage_enabled: false,
+        });
+
+        let vertex_buffer = device
+            .create_buffer_mapped(VERTICES.len(), wgpu::BufferUsage::VERTEX)
+            .fill_from_slice(VERTICES);
+        let index_buffer = device
+            .create_buffer_mapped(INDICES.len(), wgpu::BufferUsage::INDEX)
+            .fill_from_slice(INDICES);
+        let num_indices = INDICES.len() as u32;
+
+        Self {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            render_pipeline,
+            vertex_buffer,
+            index_buffer,
+            num_indices,
             diffuse_texture,
-            diffuse_buffer,
-            diffuse_sampler,
             diffuse_texture_view,
+            diffuse_sampler,
             diffuse_bind_group,
-            texture_bind_group_layout,
-        )
+            size,
+        }
     }
+
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         self.size = new_size;
         self.sc_desc.width = new_size.width;
         self.sc_desc.height = new_size.height;
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
     fn render(&mut self) {
         let frame = self.swap_chain.get_next_texture();
 
@@ -352,10 +320,7 @@ impl State {
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(PhysicalSize::new(500, 500))
-        .build(&event_loop)
-        .unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     let mut state = State::new(&window);
 
@@ -363,27 +328,34 @@ fn main() {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-            WindowEvent::KeyboardInput { input, .. } => match input {
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                    ..
-                } => *control_flow = ControlFlow::Exit,
-                _ => *control_flow = ControlFlow::Wait,
-            },
-            WindowEvent::Resized(physical_size) => {
-                state.resize(*physical_size);
+        } if window_id == window.id() => {
+            if state.input(event) {
                 *control_flow = ControlFlow::Wait;
+            } else {
+                match event {
+                    WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput { input, .. } => match input {
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        _ => *control_flow = ControlFlow::Wait,
+                    },
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                        *control_flow = ControlFlow::Wait;
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                        *control_flow = ControlFlow::Wait;
+                    }
+                    _ => *control_flow = ControlFlow::Wait,
+                }
             }
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                state.resize(**new_inner_size);
-                *control_flow = ControlFlow::Wait;
-            }
-            _ => *control_flow = ControlFlow::Wait,
-        },
+        }
         Event::MainEventsCleared => {
+            state.update();
             state.render();
             *control_flow = ControlFlow::Wait;
         }
